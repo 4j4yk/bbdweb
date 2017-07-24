49a17a48f30aec2f29e0b6440576aa8655a55b09
AK <akhampariya@unomaha.edu>
2017-07-24 15:54:47 -0400

    Navigation and page contents

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ var routes = require('./routes/index');
 
 var app = express();
 // use this if putting on heroku kind of platform and configure mongo_atlas_uri with mongo path
-var url = process.env.mongo_atlas_uri; 
+// var url = process.env.mongo_atlas_uri; 
 
 // for local connections using default port, add port if you have changed initially
-// var url = "mongodb://localhost:/nbi";
+var url = "mongodb://localhost/nbi";
+// var url = process.env.MONGODB_URI;
 mongoose.connect(url, {
-  useMongoClient: true,
-  /* other options */
+    useMongoClient: true,
+    /* other options */
 });
 
 //Get the default connection
@@ -27,10 +28,10 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
-// uncomment after placing your favicon in /public
+// un comment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -43,9 +44,9 @@ app.use('/data', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -53,23 +54,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,49 +3,74 @@ var router = express.Router();
 var Bridge = require('../models/Bridge');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('landing');
+    res.render('landing');
 });
 
-//data index page
-router.get('/data', function(req, res, next) {
-          res.render('data/index', { title: 'NBI Bridge Data'});
+//Landing page
+router.get('/home', function(req, res, next) {
+    res.render('data/index', { title: 'NBI Bridge Data' });
+});
+
+// Bridges page
+router.get('/Bridges', function(req, res, next) {
+    Bridge.find(function(err, bridges) {
+        console.log(bridges);
+        res.render('data/lookup', { title: 'NBI Bridge Data', bridges: bridges });
+    });
+});
+// Search route
+router.get('/findone/:id', function(req, res, next) {
+    console.log('getting Bridge');
+    Bridge.findOne({
+            isbn: req.params.id
+        })
+        .exec(function(err, bridges) {
+            if (err) {
+                res.send('error occurred')
+            } else {
+                console.log(bridges);
+                //  res.json(bridges);
+                res.render('data/findone', { title: 'NBI Bridge Data', bridges: bridges });
+            }
+        });
+});
+router.post('/findone/submit', function(req, res, next) {
+    var id = req.body.id;
+    res.redirect('/findone/' + id);
+});
+
+//People
+router.get('/people', function(req, res, next) {
+    res.render('data/people', { title: 'NBI Bridge Data' });
+});
+//Workshop
+router.get('/workshop', function(req, res, next) {
+    res.render('data/workshop', { title: 'NBI Bridge Data' });
+});
+// Partner
+router.get('/partner', function(req, res, next) {
+    res.render('data/partner', { title: 'NBI Bridge Data' });
 });
 
 
-//lookup page
-router.get('/lookup', function(req, res, next) {
-    Bridge.find(function(err, bridges){
-    // console.log(bridges);
-    res.render('data/lookup', { title: 'NBI Bridge Data', bridges: bridges});
-    }); 
+
+//Pilot
+router.get('/pilot', function(req, res, next) {
+    res.render('data/pilot', { title: 'NBI Bridge Data' });
+
 });
-// router.get('/lookup', function(req, res, next) {
-//   console.log('getting all books');
-//   Bridge.find({})
-//     .exec(function(err, bridges) {
-//       if(err) {
-//         res.send('error occured')
-//       } else {
-//         console.log(bridges);
-//         res.render('data/lookup', { title: 'NBI Bridge Data', bridges: bridges});
-//       }
-//     });
-// });
-
-
-
 // router.get('/lookup', function(req, res, next) {
 //   Bridge.find(function(err, BridgeRecs){
 //   res.render('data/lookup', { title: 'NBI Bridge Data', BridgeRecs: BridgeRecs});
 //   });
-  
+
 // });
 // router.get('/lookup', function(req, res, next) {
 //   Bridge.find(function(err, bridges){
 //     console.log(bridges);
 //     res.render('data/lookup', { title: bridges, bridges: bridges});
 //   });
-  
+
 // });
 // router.get('/bridge', function(req, res) {
 //   console.log('getting all books');
